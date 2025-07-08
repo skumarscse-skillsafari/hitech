591af699060b111cc30742cf7469e874d50def81
skumarscse-skillsafari <skumarscse@gmail.com>
2025-07-08 17:21:23 +0530

    added base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hitech/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
