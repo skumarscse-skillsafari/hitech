9ca2bfb23310a1e5006d8973787f7f0b2d0f89e7
skumarscse-skillsafari <skumarscse@gmail.com>
2025-07-10 22:07:31 +0530

    logo path modified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -102,7 +102,7 @@ const Header: React.FC<HeaderProps> = ({ collegeName, collegeSubtitle, navigatio
           <Link to="/" className="flex items-center space-x-3 flex-shrink-0">
             <div className="bg-white p-1 rounded-lg shadow-md">
               <img 
-                src="/Logo.jpg" 
+                src="Logo.jpg" 
                 alt="Hindusthan Institute of Technology Logo"
                 className="h-16 w-auto object-contain"
               />
@@ -121,7 +121,7 @@ const Header: React.FC<HeaderProps> = ({ collegeName, collegeSubtitle, navigatio
                 Hindusthan Institute<br />of Technology
               </div>
               <div className="text-xs font-semibold text-gray-500 leading-tight">
-                An Autonomous Institution
+                (An Autonomous Institution)
               </div>
             </div>
           </Link>
