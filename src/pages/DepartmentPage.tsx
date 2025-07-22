58d8c1d5ef872a5b078d5f49bc20346c2399342a
DevPeter01 <rohithcs1414@gmail.com>
2025-07-22 12:56:50 +0530

    edited in dept page

--- src/pages/DepartmentPage.tsx
+++ src/pages/DepartmentPage.tsx
@@ -9,6 +9,7 @@ import FacilitiesCarousel from '../components/FacilitiesCarousel';
 import TabsSection from '../components/TabsSection';
 
 import departmentsData from '../data/departmentsData.json';
+import StudentClubs from './StuClub';
 
 const DepartmentPage: React.FC = () => {
   const { departmentId } = useParams<{ departmentId: string }>();
@@ -32,7 +33,10 @@ const DepartmentPage: React.FC = () => {
           <DepartmentDetail department={department} />
         </SectionWrapper>
 
-       
+        <SectionWrapper lazy lazyHeight="300px" lazyDelay={300}>
+          <StudentClubs />
+        </SectionWrapper>
+
 
         {/* Teaching Methodologies */}
         <SectionWrapper lazy lazyHeight="500px" lazyDelay={900} className="mt-12">
