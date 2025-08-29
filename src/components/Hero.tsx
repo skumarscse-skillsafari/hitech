5afd938dc1a25911f7424ff0a242156b4c7c9b0b
DevPeter01 <rohithcs1414@gmail.com>
2025-08-29 12:19:48 +0530

    vid

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -68,7 +68,7 @@ const Hero: React.FC<HeroProps> = ({ hero }) => {
       <video
         ref={videoRef}
         className="absolute inset-0 w-full h-full object-cover"
-        src="./drone/drone2.mp4"
+        src="\drone\drone2.mp4"
         autoPlay
         loop
         muted
