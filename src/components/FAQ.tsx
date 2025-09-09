6f86da6d1c3a095e656f1766885829673c6451e1
Sridhar Dev <sridhars12082006@gmail.com>
2025-09-09 15:46:52 +0530

    small changes

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -190,7 +190,14 @@ const FAQ: React.FC = () => {
               </button>
           
 
-            <button className="border-2 border-yellow-500 text-yellow-700 hover:bg-yellow-500 hover:text-gray-900 px-8 py-3 rounded-lg font-semibold transition-all duration-300">
+            <button
+            onClick={() => {
+                 const section = document.getElementById('contact');
+                 if (section) {
+                 section.scrollIntoView({ behavior: 'smooth' });
+                 }
+                  }}
+                   className="border-2 border-yellow-500 text-yellow-700 hover:bg-yellow-500 hover:text-gray-900 px-8 py-3 rounded-lg font-semibold transition-all duration-300">
               Schedule a Campus Visit
             </button>
           </div>
